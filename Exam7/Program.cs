b3a77e45669669518bd1f997281351d675e32d60
Oix135 <oix@mail.ru>
2024-01-13 19:12:26 +0300

    dev. SringExt в отдельный файл

--- Exam7/Program.cs
+++ Exam7/Program.cs
@@ -71,11 +71,7 @@ namespace Exam7
     {
         public static List<Food> Foods { get; set; } = new List<Food>();
     }
-    public interface IProduct<TArticul>
-    {
-        public TArticul Articul { get; set; }
-    }
-
+   
     class Order<TDelivery,
     TStruct> where TDelivery : Delivery
     {
@@ -89,62 +85,5 @@ namespace Exam7
         {
             Console.WriteLine(Delivery.Address);
         }
-    }
-    public static class StringExtentions
-    {
-        public static string Genitive(this int count, Currency currency)
-        {
-            var lastDigit = count % 10;
-
-            switch (lastDigit)
-            {
-                case 0:
-                case 5:
-                case 6:
-                case 7:
-                case 8:
-                case 9:
-                    {
-                        switch (currency)
-                        {
-                            case Currency.RUB:
-                                return $"{count} рублей";
-                            case Currency.USD:
-                                return $"{count} долларов";
-                            default:
-                                return $"{count} евро";
-                        }
-                    }
-                case 1:
-                    {
-                        switch (currency)
-                        {
-                            case Currency.RUB:
-                                return $"{count} рубль";
-                            case Currency.USD:
-                                return $"{count} доллар";
-                            default:
-                                return $"{count} евро";
-                        }
-                    }
-                case 2:
-                case 3:
-                case 4:
-                    {
-                        switch (currency)
-                        {
-                            case Currency.RUB:
-                                return $"{count} рублея";
-                            case Currency.USD:
-                                return $"{count} доллара";
-                            default:
-                                return $"{count} евро";
-                        }
-                    }
-                default:
-                    return $"{count} евро";
-            }
-        }
-
     }
 }
---
+++ Exam7/StringExtentions.cs
@@ -0,0 +1,60 @@
+﻿namespace Exam7
+{
+    public static class StringExtentions
+    {
+        public static string Genitive(this int count, Currency currency)
+        {
+            var lastDigit = count % 10;
+
+            switch (lastDigit)
+            {
+                case 0:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    {
+                        switch (currency)
+                        {
+                            case Currency.RUB:
+                                return $"{count} рублей";
+                            case Currency.USD:
+                                return $"{count} долларов";
+                            default:
+                                return $"{count} евро";
+                        }
+                    }
+                case 1:
+                    {
+                        switch (currency)
+                        {
+                            case Currency.RUB:
+                                return $"{count} рубль";
+                            case Currency.USD:
+                                return $"{count} доллар";
+                            default:
+                                return $"{count} евро";
+                        }
+                    }
+                case 2:
+                case 3:
+                case 4:
+                    {
+                        switch (currency)
+                        {
+                            case Currency.RUB:
+                                return $"{count} рублея";
+                            case Currency.USD:
+                                return $"{count} доллара";
+                            default:
+                                return $"{count} евро";
+                        }
+                    }
+                default:
+                    return $"{count} евро";
+            }
+        }
+
+    }
+}
